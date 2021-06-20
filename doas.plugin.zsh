85ead17c5be37ad41fdd967dc2bb676290af4cc6
monesonn <monesonn@tuta.io>
2021-06-20 14:42:36 +0300

    update doas.plugin.zsh

--- doas.plugin.zsh
+++ doas.plugin.zsh
@@ -1,6 +1,11 @@
-# author: monesonn <git.io/monesonn>
-# description: doas before the command; triggered by double esc.
-# credit: https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/sudo/sudo.plugin.zsh
+#      __                       __
+#  ___/ /__  ___ ____  ___ ___ / /
+# / _  / _ \/ _ `(_-<_/_ /(_-</ _ \
+# \_,_/\___/\_,_/___(_)__/___/_//_/
+
+# Author: monesonn <git.io/monesonn>
+# Description: doas before the command; triggered by double esc.
+# Credit: https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/sudo/sudo.plugin.zsh
 
 __doas-replace-buffer() {
     local old=$1 new=$2 space=${2:+ }
@@ -20,28 +25,51 @@ doas-command-line() {
         WHITESPACE=" "
         LBUFFER="${LBUFFER:1}"
     fi
-    # get the first part of the typed command
-    local cmd="${${(Az)BUFFER}[1]}"
-    # check if alias
-    [[ -n $(alias $cmd) ]] && doas_cmd="doas $(alias $cmd | sed -r "s/.*='(.*)'/\1/;s/.*=(.*)/\1/")" || doas_cmd="doas $cmd"
-    # replace buffer
-    if [[ -n $cmd && $BUFFER = $cmd\ * ]]; then
-        __doas-replace-buffer "$cmd" "$doas_cmd"
-    elif [[ -n $cmd && $BUFFER = \$cmd\ * ]]; then
-        __doas-replace-buffer "\$cmd" "$doas_cmd"
-    elif [[ $BUFFER = "doas $cmd"\ * ]]; then
-        __doas-replace-buffer "$doas_cmd" "$cmd"
-    elif [[ $BUFFER = doas\ * ]]; then
+    # get the first part of the typed command and check if it's an alias to $EDITOR
+    # if so, locally change $EDITOR to the alias so that it matches below
+    if [[ -n "$EDITOR" ]]; then
+        local cmd="${${(Az)BUFFER}[1]}"
+        if [[ "${aliases[$cmd]} " = (\$EDITOR|$EDITOR)\ * ]]; then
+            local EDITOR="$cmd"
+        fi
+    fi
+    # replace buffer conditions;
+    # main roots:
+    # - doasedit, whatever implementation (there's no official one and most likely will not);
+    #             I use my own: http://git.io/doasedit
+    # - doas before associated command to a given alias
+    if [[ $(command -v doasedit) ]]; then
+      if [[ -n $EDITOR && $BUFFER = $EDITOR\ * ]]; then
+        __doas-replace-buffer "$EDITOR" "doasedit"
+      elif [[ -n $EDITOR && $BUFFER = \$EDITOR\ * ]]; then
+        __doas-replace-buffer "\$EDITOR" "doasedit"
+      elif [[ $BUFFER = doasedit\ * ]]; then
+        __doas-replace-buffer "doasedit" "$EDITOR"
+      elif [[ $BUFFER = doas\ * ]]; then
         __doas-replace-buffer "doas" ""
-    else
+      else
         LBUFFER="doas $LBUFFER"
+      fi
+    else
+      [[ -n $(alias "$cmd") ]] && {
+        LBUFFER=${$(alias $cmd)##*=};
+        LBUFFER=${LBUFFER%\'};
+        LBUFFER=${LBUFFER#\'};
+      }
+      if [[ $BUFFER = doas\ * ]]; then
+        __doas-replace-buffer "doas" ""
+      else
+        LBUFFER="doas $LBUFFER"
+      fi
     fi
     # preserve beginning space
     LBUFFER="${WHITESPACE}${LBUFFER}"
 }
 
 zle -N doas-command-line
+
 # defined shortcut keys: [esc] [esc]
 bindkey -M emacs '\e\e' doas-command-line
 bindkey -M vicmd '\e\e' doas-command-line
 bindkey -M viins '\e\e' doas-command-line
+
